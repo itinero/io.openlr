da0b92be469aa047d2bb3a4c037b93cc7402127f
Ben Abelshausen <ben@osmsharp.com>
2018-01-08 09:19:56 +0000

    Updated version #.

--- SharedAssemblyVersion.cs
+++ SharedAssemblyVersion.cs
@@ -18,5 +18,5 @@
 
 using System.Reflection;
 
-[assembly: AssemblyVersion("1.4.0")]
-[assembly: AssemblyInformationalVersion("1.4.0-pre18")]
+[assembly: AssemblyVersion("0.0.1")]
+[assembly: AssemblyInformationalVersion("0.0.1")]
